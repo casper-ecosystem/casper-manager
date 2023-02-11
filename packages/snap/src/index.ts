76f6526caa62298a26a3161eeb2b11da6dc97a5c
Killian Hascoet <killianh@live.fr>
2023-02-11 06:12:40 +0100

    Sign & Get CSPR Address

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,13 +1,120 @@
-import { OnRpcRequestHandler } from '@metamask/snap-types';
+import { OnRpcRequestHandler } from '@metamask/snaps-types';
+import { getBIP44AddressKeyDeriver } from '@metamask/key-tree';
+import * as nacl from 'tweetnacl-ts';
+import { sha256 } from 'ethereum-cryptography/sha256';
+import { ecdsaSign } from 'ethereum-cryptography/secp256k1-compat';
+import { copyable, heading, NodeType, panel, text } from '@metamask/snaps-ui';
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports,@typescript-eslint/no-var-requires,import/no-extraneous-dependencies
+globalThis.Buffer = require('buffer/').Buffer;
 
 /**
- * Get a message from the origin. For demonstration purposes only.
+ * Get casper address.
  *
- * @param originString - The origin string.
- * @returns A message based on the origin.
+ * @param addressIndex - Address index.
  */
-export const getMessage = (originString: string): string =>
-  `Hello, ${originString}!`;
+async function getCSPRAddress(addressIndex = 0) {
+  const bip44Node = await snap.request({
+    method: 'snap_getBip44Entropy',
+    params: {
+      coinType: 506,
+    },
+  });
+  const bip44Nodeaddr = await getBIP44AddressKeyDeriver(bip44Node);
+  const addressKey0 = await bip44Nodeaddr(addressIndex);
+  return {
+    publicKey: Buffer.from(addressKey0.compressedPublicKeyBytes).toString(
+      'hex',
+    ),
+    curve: addressKey0.curve,
+  };
+}
+
+/**
+ * Displays a prompt to the user in the MetaMask UI.
+ *
+ * @param deployInfo - Object that represent the info of the deploy.
+ * @returns `true` if the user accepted the confirmation,
+ * and `false` otherwise.
+ */
+async function promptUserDeployInfo(deployInfo: any) {
+  const deployArgComponents: {
+    value: string;
+    type: NodeType.Text | NodeType.Copyable;
+  }[] = [];
+  Object.entries(deployInfo.deployArgs).forEach((arg: any) => {
+    deployArgComponents.push(text(arg[0]), copyable(arg[1]));
+  });
+  return await snap.request({
+    method: 'snap_dialog',
+    params: {
+      type: 'Confirmation',
+      content: panel([
+        heading(`Sign ${deployInfo.deployType}`),
+        text('Deploy Hash'),
+        copyable(deployInfo.deployHash),
+        text('Signing Key'),
+        copyable(deployInfo.signingKey),
+        text('Account'),
+        copyable(deployInfo.account),
+        text('Body Hash'),
+        copyable(deployInfo.bodyHash),
+        text('Chain Name'),
+        copyable(deployInfo.chainName),
+        text('Timestamp'),
+        copyable(deployInfo.timestamp),
+        text('Gas'),
+        copyable(deployInfo.payment),
+        panel([heading('Deploy Arguments'), ...deployArgComponents]),
+      ]),
+    },
+  });
+}
+
+/**
+ * Sign a message.
+ *
+ * @param deployInfo - Confirmation message.
+ * @param msg - Message.
+ * @param addressIndex - Address index.
+ */
+async function sign(deployInfo: any, msg: string, addressIndex = 0) {
+  const bip44Node = await snap.request({
+    method: 'snap_getBip44Entropy',
+    params: {
+      coinType: 506,
+    },
+  });
+  const message = Buffer.from(msg, 'hex');
+  const bip44Nodeaddr = await getBIP44AddressKeyDeriver(bip44Node);
+  const addressKey0 = await bip44Nodeaddr(addressIndex);
+  const response = await promptUserDeployInfo(deployInfo);
+  if (!response) {
+    return false;
+  }
+
+  if (addressKey0.privateKeyBytes) {
+    if (addressKey0.curve === 'ed25519') {
+      return {
+        signature: Buffer.from(
+          nacl.sign_detached(message, addressKey0.privateKeyBytes),
+        ).toString('hex'),
+      };
+    }
+
+    if (addressKey0.curve === 'secp256k1') {
+      const res = ecdsaSign(
+        sha256(Buffer.from(message)),
+        addressKey0.privateKeyBytes,
+      );
+      return { signature: Buffer.from(res.signature).toString('hex') };
+    }
+    return {
+      error: `Unsupported curve : ${addressKey0.curve}. Only Secp256K1 && Ed25519 are supported.`,
+    };
+  }
+  return { error: 'No private key associated with the account.' };
+}
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -20,21 +127,20 @@ export const getMessage = (originString: string): string =>
  * @throws If the request method is not valid for this snap.
  * @throws If the `snap_confirm` call failed.
  */
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+export const onRpcRequest: OnRpcRequestHandler = async ({
+  origin,
+  request,
+}) => {
+  console.log(origin, request);
   switch (request.method) {
-    case 'hello':
-      return wallet.request({
-        method: 'snap_confirm',
-        params: [
-          {
-            prompt: getMessage(origin),
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
-      });
+    case 'casper_getAccount':
+      return getCSPRAddress(request?.params?.addressIndex);
+    case 'casper_sign':
+      return sign(
+        request?.params?.deployInfo,
+        request?.params?.message,
+        request?.params?.addressIndex,
+      );
     default:
       throw new Error('Method not found.');
   }
